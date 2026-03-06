8acd377fee03566ea3677a58186008d7fff89d1b
CamiSerantoni <camila.serantonir@gmail.com>
2026-03-06 17:17:40 -0300

    first commit whith base code

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Header } from './components/Header'
+import { Guitar } from './components/Guitar'
+
+
+function App() {
+
+  //state 
+  const [auth, setAuth] = useState(false);
+
+
+
+  return (
+    <>
+    
+    <Header/>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+           
+    <Guitar/>
+           
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+     
+    </>
+  )
+}
+
+export default App
